a995ef4a13293f6a08f2f23a03574a7535db1d2b
Michael <mjohnson9@sheffield.ac.uk>
2021-11-13 23:57:46 +0000

    Handling subtitle extraction from file

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ fn main() {
     let _args: Vec<String> = env::args().collect();
     let filepaths = &_args[1..];
     for file in filepaths{
-        get_subtitle(file);
+        let subtitles = ffprobe_get_subtitle_sources(file);
+        ffmpeg_subtitle_extractor(subtitles,file);
     }
 }
 
-fn get_subtitle(file:&String){
-
+fn ffprobe_get_subtitle_sources(file:&String) -> Vec<String>{
     let metadata = process::Command::new("ffprobe")
         .arg("-loglevel")
         .arg("error")
@@ -24,19 +24,26 @@ fn get_subtitle(file:&String){
         .output()
         .unwrap();
 
-    let echo = String::from_utf8(metadata.stdout).unwrap();
-    let v: Vec<&str> = echo.split('\n').collect();
-    println!("{:?}",v);
+    let subtitle_string: String = String::from_utf8(metadata.stdout).unwrap();
+    let v: Vec<String> = subtitle_string.split("\n").map(|s| s.to_string()).collect();
+    return v
 
-    //let subtitle_grabber = process::Command::new("ffmpeg")
-    //    .arg("-i")
-    //    .arg(file.to_string())
-    //    .arg("-map")
-    //    .arg("0:s:0")
-    //    .arg("/home/iggy/Documents/subs.srt");
+}
 
-
-
-
+fn ffmpeg_subtitle_extractor(source_list:Vec<String>,file:&String){
+    for source in source_list{
+        let t:Vec<&str> = source.split(',').collect(); 
+        let stream = t[0];
+        let name = t[1];
+        let stream_info = format!("0:{}",stream);
+        let filename = format!("/home/iggy/Documents/{}.srt",name);
+        process::Command::new("ffmpeg")
+            .arg("-i")
+            .arg(file.to_string())
+            .arg("-map")
+            .arg(stream_info)
+            .arg(filename)
+            .spawn().unwrap();
+    }
 
 }
