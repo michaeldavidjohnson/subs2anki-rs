059c44f44b0d4b9c190f227b3470134736bd1d85
Michael <mjohnson9@sheffield.ac.uk>
2021-12-28 19:08:38 +0000

    Added actual user input handling, can extract subs on request, and can split videos and subtitles together. Just need to handle splitting when multiple videos and subtitles are asked

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::process;
 use std::fs;
 use regex::Regex;
 
+const ACCEPTED_SUBTITLE_EXTENSIONS: [&str ;1] = ["srt"];
+const ACCEPTED_VIDEO_EXTENSIONS: [&str ;3] = ["mkv","mp4","avi"];
+const TEMP_FILEPATH: &str = "/home/iggy/Documents/Rust/subs2srsclone/Temp/";
+
 fn main() {
     let _args: Vec<String> = env::args().collect();
 
@@ -14,13 +18,16 @@ fn main() {
 
     //Logic for separating inputs into subtitle files and video files, this will become the backbone of being able to call the other commands.
     //We now just need a way to zip the two vecs together to performe subtitle splitting, screenshot generation, etc.
-    let accepted_subtitle_extensions = ["srt","why"];
-    let accepted_video_extensions = ["mkv","mp4","avi"];
     let filepaths = &_args[1..];
     let mut subtitles = Vec::<&String>::new();
     let mut videos = Vec::<&String>::new();
+    let mut extractor_toggle: bool = false;
     for file in filepaths{
-        for sub in accepted_subtitle_extensions{
+        if file == "__extract" || file == "_e"{
+            extractor_toggle = true;
+        }
+
+        for sub in ACCEPTED_SUBTITLE_EXTENSIONS{
             let last_three: Vec<char> = file.chars().rev().take(3).collect();
             let last_three_subtitle: Vec<char> = sub.chars().rev().take(3).collect();
             let subtitle_condition = last_three_subtitle.into_iter().rev().collect::<String>() == last_three.into_iter().rev().collect::<String>();
@@ -29,7 +36,7 @@ fn main() {
 
             }
         }
-        for viddy in accepted_video_extensions{
+        for viddy in ACCEPTED_VIDEO_EXTENSIONS{
             let last_three: Vec<char> = file.chars().rev().take(3).collect();
             let last_three_video: Vec<char> = viddy.chars().rev().take(3).collect();
             let video_condition = last_three_video.into_iter().rev().collect::<String>() == last_three.into_iter().rev().collect::<String>();
@@ -38,28 +45,35 @@ fn main() {
             }
         }
     }
-    for val in &subtitles{
-        println!("{}",val);
-    }
-    for val in &videos{
-        println!{"{}",val};
+
+    if &subtitles.len()== &videos.len(){
+        populate_information_and_screenshots(&subtitles, &videos);
     }
 
-    //Subtitle splitting, again we really need user input here to decide which functions to use instead of commenting out bad functions.
-    //for file in filepaths{
-    //    let (start_times, end_times, text) = read_subtitles_and_split(file);
-    //    let formatted_text = format_text_to_remove_html(text);
-    //    ffmpeg_generate_screenshots(&start_times,&end_times);
-    //}
+    if subtitles.len() == 0 as usize && extractor_toggle == true{
+        println!("{:?}",&videos);
+        subtitle_extraction(&videos);
+    }
 
 
-    //Subtitle extraction, doesn't really have any handling of if you already have the files or not. Really missing some form of
-    //consistent user input.
-    //for file in filepaths{
-    //    let subtitles = ffprobe_get_subtitle_sources(file);
-    //    ffmpeg_subtitle_extractor(subtitles.clone(),file);
-    //    create_subtitle_metadata(subtitles.clone());
-    //}
+}
+
+
+fn populate_information_and_screenshots(subtitle_files: &Vec<&String>,video_files: &Vec<&String>) {
+    for i in 0..subtitle_files.len(){
+        let (start_times, end_times, text) = read_subtitles_and_split(subtitle_files[i]);
+        let formatted_text = format_text_to_remove_html(text);
+        ffmpeg_generate_screenshots(&start_times,&end_times,&i,&video_files[i]);
+    }
+}
+
+//Needs user input to select subtitles
+fn subtitle_extraction(video_files: &Vec<&String>){
+    for file in video_files {
+        let subtitles = ffprobe_get_subtitle_sources(file);
+        ffmpeg_subtitle_extractor(subtitles.clone(),file);
+        create_subtitle_metadata(subtitles.clone());
+    }
 }
 
 fn ffprobe_get_subtitle_sources(file:&String) -> Vec<String>{
@@ -90,30 +104,30 @@ fn ffmpeg_subtitle_extractor(source_list:Vec<String>,file:&String){
         let stream = t[0];
         let name = t[1];
         let stream_info = format!("0:{}",stream);
-        let filename = format!("/home/iggy/Documents/{}.srt",name);
+        let filename = format!("{}{}.srt",TEMP_FILEPATH,name);
         process::Command::new("ffmpeg")
             .arg("-hide_banner")
             .arg("-loglevel")
             .arg("error")
+            .arg("-n")
             .arg("-i")
             .arg(file.to_string())
             .arg("-map")
             .arg(stream_info)
             .arg(filename)
-            .spawn().unwrap();
+            .spawn().unwrap().wait().unwrap();
     }
 
 }
 
-fn ffmpeg_generate_screenshots(start_time:&Vec<String>,end_time:&Vec<String>){ //This definitely will need to take the video as an arg
-    let temp_video = "/run/media/iggy/Seagate Backup Plus Drive/Anime/Bakuman/S1/(Hi10)_Bakuman_-_01_(BD_720p)_(Judgment)_(E9EA961E).mkv";
+fn ffmpeg_generate_screenshots(start_time:&Vec<String>,end_time:&Vec<String>,file_number:&usize,video_file:&str){ 
+    let temp_video = video_file;
 
-    let temp_location = "/home/iggy/Documents/Rust/subs2srsclone/Temp/"; //This'll need to be a config thing
     let start_times = start_time;
     let end_times = end_time;
     let mut index = 0;
     for i in start_times.iter(){
-        let out_path = format!{"{}{}.jpg",temp_location,index};
+        let out_path = format!{"{}{}.jpg",TEMP_FILEPATH,index};
         let mut removed = i.to_string().clone();
         let mut sub_index = 0;
         let mut sub_string:Vec<char> = Vec::new();
